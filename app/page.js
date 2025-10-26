480e807d52d4cfed2016a68646b6645fc5afce7f
Veronica <vero@MacBook-Pro-de-Guillermo.local>
2025-10-25 19:44:30 -0600

    info banners

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Banner from "./components/Banner";
+import InfoBanner from "./components/InfoBanner";
 import "./home/style.css";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="">
       <main className="">
         <Banner />
+        <InfoBanner />
       </main>
     </div>
   );
